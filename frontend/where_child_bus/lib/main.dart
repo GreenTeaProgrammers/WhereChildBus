ea502911848d2543bbf91ef4b9a6575813bb6947
xyzyxJP <sahashi.yuto.21@shizuoka.ac.jp>
2024-02-10 13:19:14 +0900

    feat(frontend): 初期画面の作成

--- frontend/where_child_bus/lib/main.dart
+++ frontend/where_child_bus/lib/main.dart
@@ -1,68 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:where_child_bus/pages/bus_list_page/bus_list_page.dart';
+import 'package:where_child_bus/pages/notification_page/notification_page.dart';
+import 'package:where_child_bus/pages/student_list_page/student_list_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'WhereChildBus',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        title: 'WhereChildBus',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text(['園児一覧', '送迎バスコース一覧', '連絡情報設定'][_selectedIndex]),
+            ),
+            body: [
+              const StudentListPage(),
+              const BusListPage(),
+              const NotificationPage()
+            ][_selectedIndex],
+            bottomNavigationBar: BottomNavigationBar(
+              currentIndex: _selectedIndex,
+              onTap: (int index) => setState(() => _selectedIndex = index),
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.people),
+                  label: '園児一覧',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.directions_bus),
+                  label: '送迎バスコース一覧',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.notifications),
+                  label: '連絡情報設定',
+                ),
+              ],
+            )));
   }
 }
---
+++ frontend/where_child_bus/lib/pages/bus_list_page/bus_list_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class BusListPage extends StatefulWidget {
+  const BusListPage({super.key});
+
+  @override
+  State<BusListPage> createState() => _BusListPageState();
+}
+
+class _BusListPageState extends State<BusListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            '送迎バスコース一覧',
+          ),
+        ],
+      ),
+    );
+  }
+}
