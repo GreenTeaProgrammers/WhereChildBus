0fb6703955d63479fd44e7065dff6034d146979c
KikyoNanakusa <nanakusa.vrc@gmail.com>
2024-02-10 17:09:02 +0900

    feat[表示]:バスのリスト表示を追加

--- frontend/where_child_bus/lib/pages/bus_list_page/bus_list_page.dart
+++ frontend/where_child_bus/lib/pages/bus_list_page/bus_list_page.dart
@@ -8,15 +8,100 @@ class BusListPage extends StatefulWidget {
 }
 
 class _BusListPageState extends State<BusListPage> {
+  final items = ["バス1", "バス2", "バス3", "バス4", "バス5", "バス5", "バス5", "バス5", "バス5"];
+
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Text(
-            '送迎バスコース一覧',
-          ),
+          pageTitle(),
+          Expanded(
+            child: listViewBuilder(),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget pageTitle() {
+    return const Padding(
+      padding:  EdgeInsets.all(10),
+      child:  Text(
+        '送迎バスコース一覧',
+      ),
+    );
+  }
+
+  Widget listViewBuilder() {
+    return ListView.builder(
+      //TODO: 実際にはAPIからデータを取得
+      itemCount: items.length,
+      itemBuilder: (BuildContext context, int index) {
+        return busListCard(items[index]);
+      },
+    );
+  }
+
+  Widget busListCard(String name) {
+    return Card(
+      elevation: 4,
+      margin: const EdgeInsets.all(16),
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      child: busNameAndDescription(name),
+    );
+  }
+
+  Widget busPhoto() {
+    return SizedBox(
+      width: 100,
+      height: 100,
+      child: Card(
+        child: Text("ここに画像が来る")
+      ),
+    );
+  }
+
+  Widget busName(name) {
+    return Container(
+      width: double.infinity,
+      padding:
+          const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+      child: Text(
+        name,
+        style: const TextStyle(
+          fontWeight: FontWeight.w600,
+          fontSize: 22,
+        ),
+      ),
+    );
+  }
+
+  Widget busDescription(String description) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child:  Text(
+        description,
+        style: const TextStyle(
+          color: Colors.grey,
+          fontSize: 16,
+        ),
+        overflow: TextOverflow.ellipsis,
+        maxLines: 2,
+      ),
+    );
+  }
+
+  Widget busNameAndDescription(String name) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        children: [
+          busName(name),
+          busDescription("テストの説明文")
         ],
       ),
     );
