bdcf3c8dbe3303cfef1f7b80af9b6564cf002512
Mizuki <mbaba@kanolab.net>
2024-02-10 16:06:13 +0900

    chore(ml): config.yamlを想定したモデル定義への変更

--- machine_learning/src/face_detect_model/model/model.py
+++ machine_learning/src/face_detect_model/model/model.py
@@ -2,16 +2,28 @@ import torch.nn as nn
 
 
 class Net(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, config: dict, num_classes: int):
         super(Net, self).__init__()
+        self.config = config
+
         self.relu = nn.ReLU()
-        self.pool = nn.MaxPool2d(2, stride=2)
+        self.pool = nn.MaxPool2d(
+            kernel_size=config["model"]["kernel_size"], stride=config["model"]["stride"]
+        )
 
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3)
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3)
+        self.conv1 = nn.Conv2d(
+            in_channels=1,
+            out_channels=16,
+            kernel_size=config["model"]["Conv2d"]["kernel_size"],
+        )
+        self.conv2 = nn.Conv2d(
+            in_channels=16,
+            out_channels=32,
+            kernel_size=config["model"]["Conv2d"]["kernel_size"],
+        )
 
-        self.linear = nn.Linear(32 * 5 * 5, 120)
-        self.classier = nn.Linear(120, num_classes)
+        self.linear = nn.Linear(32 * 5 * 5, config["model"]["hidden_size"])
+        self.classier = nn.Linear(config["model"]["hidden_size"], num_classes)
 
     def forward(self, x):
         x = self.conv1(x)
