86df211eee78b43fc1d3619a78b47011a15e43ea
KinjiKawaguchi <kawakin0310@icloud.com>
2024-02-11 19:21:41 +0900

    chore: grpcClient一部処理の変更

--- frontend/where_child_bus/lib/util/health_check.dart
+++ frontend/where_child_bus/lib/util/health_check.dart
@@ -3,17 +3,18 @@ import 'dart:developer' as developer; // エラーログに使用
 import 'package:grpc/grpc.dart';
 import 'package:where_child_bus/proto-gen/where_child_bus/v1/health_check.pbgrpc.dart';
 
-
 Future<PingResponse> createHoge() async {
   final channel = ClientChannel(
     'where-child-bus-grpc-k3dkun2lpq-uw.a.run.app',
     port: 443,
-    options: const ChannelOptions(credentials: ChannelCredentials.insecure()),
   );
-  final grpcClient = HealthcheckServiceClient(channel, options: CallOptions(timeout: const Duration(seconds: 10)));
+  final grpcClient = HealthcheckServiceClient(channel,
+      options: CallOptions(timeout: const Duration(seconds: 60)));
 
   try {
-    PingResponse res = await grpcClient.ping(PingRequest()..name = "PING");
+    var rqst = PingRequest();
+    rqst.name = "ping";
+    PingResponse res = await grpcClient.ping(rqst);
     await channel.shutdown();
     return Future<PingResponse>.value(res);
   } catch (error) {
