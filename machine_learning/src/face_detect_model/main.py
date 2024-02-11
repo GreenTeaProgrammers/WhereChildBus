3f8790d68a1ff5950918a5d8ca84512af00bdf03
Mizuki <mbaba@kanolab.net>
2024-02-12 01:56:40 +0900

    feat(ml): Trainerの定義，学習及びモデルに対するテスト用のコードの追加

--- machine_learning/src/face_detect_model/main.py
+++ machine_learning/src/face_detect_model/main.py
@@ -1,1 +1,65 @@
+import yaml
+import argparse
+import torch
 
+from data.faceDetectDataset import FaceDetectDataset
+from model.faceDetectModel import FaceDetectModel
+from trainer import Trainer
+
+
+def main(args: argparse.Namespace, config: dict):
+    args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # datasetの読み込み
+    dataset = FaceDetectDataset(config)
+
+    train_data_size = int(len(dataset) * config["dataset"]["ratio"]["train"])
+    valid_data_size = int(len(dataset) * config["dataset"]["ratio"]["valid"])
+    test_data_size = int(len(dataset) * config["dataset"]["ratio"]["test"])
+    train_data_size = len(dataset) - valid_data_size - test_data_size
+
+    train_dataset, valid_dataset, test_dataset = torch.utils.data.random_split(
+        dataset,
+        [
+            train_data_size,
+            valid_data_size,
+            test_data_size,
+        ],
+    )
+
+    # modelの読み込み
+    num_classes = dataset.get_label_num()
+    image_shape = dataset.get_image_shape()
+    face_detect_model = FaceDetectModel(config, num_classes, image_shape)
+    face_detect_model.to(args.device)
+
+    # trainerの読み込み
+    # 学習
+    if args.mode == "train":
+        face_detect_model = face_detect_model.train()
+        trainer = Trainer(
+            args, config, face_detect_model, train_dataset, valid_dataset, test_dataset
+        )
+        trainer.train()
+    else:
+        trainer = Trainer(
+            args, config, face_detect_model, train_dataset, valid_dataset, test_dataset
+        )
+        trainer.test()
+
+    # テスト
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--config", type=str, default="src/face_detect_model/config.yaml"
+    )
+    parser.add_argument("--mode", type=str, required=True, choices=["train", "test"])
+    parser.add_argument("--debug", action="store_true")
+    args = parser.parse_args()
+
+    with open(args.config, "r") as f:
+        config = yaml.safe_load(f)
+
+    main(args, config)
--- machine_learning/src/face_detect_model/trainer.py
+++ machine_learning/src/face_detect_model/trainer.py
@@ -1,1 +1,141 @@
+import os
+import torch
+import torch.nn as nn
+import argparse
 
+from data.faceDetectDataset import FaceDetectDataset
+from model.faceDetectModel import FaceDetectModel
+
+
+class Trainer:
+    def __init__(
+        self,
+        args: argparse.Namespace,
+        config: dict,
+        model: FaceDetectModel,
+        train_dataset: FaceDetectDataset,
+        valid_dataset: FaceDetectDataset,
+        test_dataset: FaceDetectDataset,
+    ):
+        self.args = args
+        self.config = config
+        self.model = model
+
+        self.criterion = torch.nn.CrossEntropyLoss()
+        self.optimizer = torch.optim.Adam(
+            self.model.parameters(), lr=config["train"]["learning_rate"]
+        )
+        self.softmax = nn.Softmax(dim=1)
+
+        self.epoch = 1
+        self.step_num = 1
+
+        self.best_loss = float("inf")
+
+        self.train_dataset = train_dataset
+        self.valid_dataset = valid_dataset
+        self.test_dataset = test_dataset
+
+        self.train_dataloader = torch.utils.data.DataLoader(
+            self.train_dataset,
+            batch_size=config["train"]["train_batch_size"],
+            shuffle=True,
+        )
+        self.valid_dataloader = torch.utils.data.DataLoader(
+            self.valid_dataset,
+            batch_size=config["train"]["valid_batch_size"],
+            shuffle=False,
+        )
+        self.test_dataloader = torch.utils.data.DataLoader(
+            self.test_dataset,
+            batch_size=config["train"]["test_batch_size"],
+            shuffle=False,
+        )
+
+        if not os.path.exists(config["train"]["save_model_path"]):
+            os.makedirs(config["train"]["save_model_path"])
+
+    def train(self):
+        for _ in range(self.config["train"]["epoch"]):
+            for label, image in self.train_dataloader:
+                self.step(label, image)
+            self.end_epoch()
+            if self.epoch % self.config["train"]["validate_interval"] == 0:
+                self.validate()
+        self.test()
+
+    def step(self, label: torch.Tensor, image: torch.Tensor):
+        self.optimizer.zero_grad()
+
+        output = self.model(image.to(self.args.device))
+
+        loss = self.criterion(output, label)
+        loss.backward()
+        self.optimizer.step()
+
+        self.last_loss = loss.item()
+        self.step_num += 1
+
+    def end_epoch(self):
+        print(f"Epoch {self.epoch} Loss: {self.last_loss}")
+
+        if self.last_loss < self.best_loss:
+            self.best_loss = self.last_loss
+            self.save_model(
+                os.path.join(
+                    self.config["train"]["save_model_path"],
+                    "best_model.pth",
+                )
+            )
+
+        self.epoch += 1
+        self.step_num = 1
+
+    def validate(self):
+        collect_list = []
+        pred_list = []
+        for label, image in self.valid_dataloader:
+            output = self.model(image.to(self.args.device))
+            output = self.softmax(output)
+
+            pred = torch.argmax(output, 1)
+
+            pred_list.append(pred)
+            collect_list.append(label)
+
+        if self.args.debug:
+            print(f"Collect: {collect_list}")
+            print(f"Predict: {pred_list}")
+        accuracy = self.calc_accuracy(pred_list, collect_list)
+        print(f"########## Epoch {self.epoch} ##########")
+        print(f"Validation Accuracy: {accuracy}")
+
+    def test(self):
+        self.model.eval()
+        collect_list = []
+        pred_list = []
+        for label, image in self.test_dataloader:
+            with torch.no_grad():
+                output = self.model(image.to(self.args.device))
+            output = self.softmax(output)
+
+            pred = torch.argmax(output, 1)
+
+            pred_list.append(pred)
+            collect_list.append(label)
+
+        if self.args.debug:
+            print(f"Collect: {collect_list}")
+            print(f"Predict: {pred_list}")
+        accuracy = self.calc_accuracy(pred_list, collect_list)
+        print("########## Test ##########")
+        print(f"Test Accuracy: {accuracy}")
+
+    def calc_accuracy(self, pred_list, collect_list):
+        pred = torch.cat(pred_list)
+        collect = torch.cat(collect_list)
+        accuracy = (pred == collect).sum().item() / len(collect)
+        return accuracy
+
+    def save_model(self, save_path: str):
+        torch.save(self.model.state_dict(), save_path)
