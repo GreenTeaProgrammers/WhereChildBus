0774318c0e595e57bb007fda0e11597b4264504c
Takuya Kataiwa <takuyakataiwa@icloud.com>
2024-02-11 20:48:57 +0900

    feat: add augmatation by torchvision

--- machine_learning/src/face_detect_model/data/faceDetectDataset.py
+++ machine_learning/src/face_detect_model/data/faceDetectDataset.py
@@ -10,7 +10,7 @@ class FaceDetectDataset(Dataset):
 
     def __init__(self, config, transform=None):
         self.data_dir = config["dataset"]["root_path"]
-        self.transform = transform
+        self.transform = transform if transform is not None else self.default_transforms()
         self.face_data = []
         self.label_name_dict = {}
         name_label_dict = {}
@@ -26,22 +26,27 @@ class FaceDetectDataset(Dataset):
             image = cv2.imread(file_path)
             if image is None:
                 continue
-            # BGRからRGBに変換
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            # 画像をPIL Imageに変換（transformにPIL Imageを要求する場合）
-            # image = Image.fromarray(image)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # BGRからRGBに変換
 
-            # 人物名とラベルの対応を辞書に保存
             if name_label_dict.get(people_name) is None:
                 self.label_name_dict[self.label_num] = people_name
                 name_label_dict[people_name] = self.label_num
                 self.label_num += 1
 
-            # label_img_dataの代わりに、(label, image)のタプルを直接face_dataに追加
             self.face_data.append((
                 torch.tensor(name_label_dict[people_name], dtype=torch.int64),
                 image,  # この時点ではまだNumPy配列
             ))
+
+    @staticmethod
+    def default_transforms():
+        return transforms.Compose([
+            transforms.ToPILImage(),  # NumPy配列をPIL Imageに変換
+            transforms.Resize((224, 224)),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
 
     def _is_valid_file(self, file_name):
         return os.path.splitext(file_name)[1].lower() in self.VALID_EXTENSIONS
@@ -52,7 +57,7 @@ class FaceDetectDataset(Dataset):
     def __getitem__(self, idx):
         label, image = self.face_data[idx]
 
-        # transformが定義されている場合はここで適用
+        # NumPy配列をPIL Imageに変換し、transformが定義されている場合は適用
         if self.transform:
             image = self.transform(image)
 
@@ -65,7 +70,6 @@ class FaceDetectDataset(Dataset):
         return self.label_num
 
     def get_image_shape(self):
-        # 最初の画像の形状を取得するために変換後のTensorを使用
         if len(self.face_data) > 0:
             _, sample_image = self.face_data[0]
             if self.transform:
