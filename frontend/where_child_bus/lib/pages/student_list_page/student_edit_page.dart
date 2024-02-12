adacdf3bcdfda0ebc4566ab66760c4c7c63cb02a
koto623 <akan.02toko@gmail.com>
2024-02-12 15:33:05 +0900

    feat:editページへの遷移を追加

--- frontend/where_child_bus/lib/pages/student_list_page/student_edit_page.dart
+++ frontend/where_child_bus/lib/pages/student_list_page/student_edit_page.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 
-class StudentEditPage extends StatefulWidget {
-  const StudentEditPage({super.key});
+class StudentEditPage extends StatelessWidget {
+  final String childName;
 
-  @override
-  State<StudentEditPage> createState() => _StudentEditPage();
-}
+  const StudentEditPage({super.key, required this.childName});
 
-class _StudentEditPage extends State<StudentEditPage> {
   @override
   Widget build(BuildContext context) {
     return const Center(
-        child: Column(children: <Widget>[
-      Text("編集画面"),
-    ]));
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+          Text(
+            "編集画面",
+            style: TextStyle(color: Colors.black, fontSize: 15),
+          ),
+        ]));
   }
 }
