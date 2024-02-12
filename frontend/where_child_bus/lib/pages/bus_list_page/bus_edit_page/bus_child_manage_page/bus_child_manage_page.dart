308108301d4951a7e28f5b4a5400601c3694dba6
KikyoNanakusa <nanakusa.vrc@gmail.com>
2024-02-13 02:18:13 +0900

    feat: bus_child_manage_page.dartを追加

---
+++ frontend/where_child_bus/lib/pages/bus_list_page/bus_edit_page/bus_child_manage_page/bus_child_manage_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class BusChildManagePage extends StatefulWidget {
+
+  _BusChildManagePage createState() => _BusChildManagePage();
+}
+
+class _BusChildManagePage extends State<BusChildManagePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    ); 
+  }
+}
