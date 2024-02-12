4c9c186b6b5077f36a7a11ffa06f82269a676bee
xyzyxJP <sahashi.yuto.21@shizuoka.ac.jp>
2024-02-13 01:08:33 +0900

    feat(frontend): 保護者向けのアプリを作成

---
+++ frontend/where_child_bus_guardian/lib/pages/map_page/map_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            '地図',
+          ),
+        ],
+      ),
+    );
+  }
+}
