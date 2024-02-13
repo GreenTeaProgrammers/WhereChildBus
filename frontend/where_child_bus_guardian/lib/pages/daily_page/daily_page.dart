9bc267a54dea5ffeb37b04b393e6d29296d4c0ff
koto623 <akan.02toko@gmail.com>
2024-02-14 02:18:52 +0900

    feat:園児の写真と表情、氏名の追加

--- frontend/where_child_bus_guardian/lib/pages/daily_page/daily_page.dart
+++ frontend/where_child_bus_guardian/lib/pages/daily_page/daily_page.dart
@@ -17,10 +17,64 @@ class _DailyPageState extends State<DailyPage> {
         children: <Widget>[
           CurrentTimeBody(),
           const SizedBox(
-            height: 50,
+            height: 20,
           ),
+          childDailyRecordBody()
         ],
       ),
+    );
+  }
+
+  Widget childDailyRecordBody() {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          childFaceAndExpression(),
+          Padding(
+              padding: EdgeInsets.only(top: 20, bottom: 20),
+              child: childName()),
+        ],
+      ),
+    );
+  }
+
+  Widget childFaceAndExpression() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[childFaceImage(), childExpressionIcon()],
+    );
+  }
+
+  //TODO: 将来的に画像を受け取る
+  Widget childFaceImage() {
+    return const SizedBox(
+      width: 150,
+      height: 150,
+      child: Card(
+        color: Colors.grey,
+        child: Text("ここに子供の写真が入る"),
+      ),
+    );
+  }
+
+  //TODO: 将来的に表情を受け取る
+  Widget childExpressionIcon() {
+    return const SizedBox(
+      width: 100,
+      height: 100,
+      child: Card(
+        color: Colors.grey,
+        child: Text("ここに表情のアイコンが入る"),
+      ),
+    );
+  }
+
+  //将来的に名前を受け取る
+  Widget childName() {
+    return const Text(
+      "園児1",
+      style: TextStyle(fontSize: 24),
+      textAlign: TextAlign.center,
     );
   }
 }
