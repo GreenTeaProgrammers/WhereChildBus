679fa7876148772ff70654adc7ec6c453c91f3a4
koto623 <akan.02toko@gmail.com>
2024-02-13 11:45:33 +0900

    move:保存ボタンをutilsフォルダへ移動

--- frontend/where_child_bus/lib/pages/student_list_page/compornents/utils/submit_button.dart
+++ frontend/where_child_bus/lib/pages/student_list_page/compornents/utils/submit_button.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class SubmitButton extends StatelessWidget {
   final VoidCallback onPressed;
 
-  const SubmitButton({required this.onPressed});
+  const SubmitButton({required this.onPressed, super.key});
 
   @override
   Widget build(BuildContext context) {
--- frontend/where_child_bus/lib/pages/student_list_page/student_edit_page.dart
+++ frontend/where_child_bus/lib/pages/student_list_page/student_edit_page.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import './compornents/submit_button.dart';
+import 'compornents/utils/submit_button.dart';
 
 class StudentEditPage extends StatefulWidget {
   const StudentEditPage({super.key});
