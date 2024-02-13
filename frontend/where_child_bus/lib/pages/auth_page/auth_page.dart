e900729fb8856e238143119330e4d14641add255
KikyoNanakusa <nanakusa.vrc@gmail.com>
2024-02-14 00:56:07 +0900

    refactor:ウィジェットを分離

--- frontend/where_child_bus/lib/pages/auth_page/auth_page.dart
+++ frontend/where_child_bus/lib/pages/auth_page/auth_page.dart
@@ -18,60 +18,88 @@ class _AuthPageState extends State<AuthPage> {
     return GestureDetector(
       onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(32),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'WhereChildBus',
-                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 32),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'メールアドレス',
-                    ),
-                    controller: _emailController,
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'パスワード',
-                    ),
-                    controller: _passwordController,
-                    obscureText: true,
-                    keyboardType: TextInputType.visiblePassword,
-                  ),
-                ),
-                const SizedBox(height: 32),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.6,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (kDebugMode) {
-                        debugPrint("email: ${_emailController.text}");
-                        debugPrint("password: ${_passwordController.text}");
-                      }
-                      nurseryLogin(
-                          _emailController.text, _passwordController.text);
-                    },
-                    child: const Text('ログイン'),
-                  ),
-                )
-              ],
-            ),
-          ),
+        body: pageBody(),
+      ),
+    );
+  }
+
+  Widget pageBody() {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            titleText(),
+            mailInputField(),
+            passwordInputField(),
+            spacer32(),
+            loginButton(),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget titleText() {
+    return const Padding(
+      padding: EdgeInsets.only(bottom: 32),
+      child: Text(
+        'WhereChildBus',
+        style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  Widget mailInputField() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        decoration: const InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'メールアドレス',
+        ),
+        controller: _emailController,
+        keyboardType: TextInputType.emailAddress,
+      ),
+    );
+  }
+
+  Widget passwordInputField() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        decoration: const InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'パスワード',
+        ),
+        controller: _passwordController,
+        obscureText: true,
+        keyboardType: TextInputType.visiblePassword,
+      ),
+    );
+  }
+
+  Widget loginButton() {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.6,
+      child: ElevatedButton(
+        onPressed: () {
+          if (kDebugMode) {
+            debugPrint("email: ${_emailController.text}");
+            debugPrint("password: ${_passwordController.text}");
+          }
+          nurseryLogin(
+              _emailController.text, _passwordController.text);
+        },
+        child: const Text('ログイン'),
+      ),
+    );
+  }
+
+  Widget spacer32() {
+    return const SizedBox(
+      height: 32,
     );
   }
 }
