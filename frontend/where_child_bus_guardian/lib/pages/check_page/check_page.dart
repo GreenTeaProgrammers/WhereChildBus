3b333df9a9970c315fbc41a6976887f78ea8ce19
koto623 <akan.02toko@gmail.com>
2024-02-13 18:15:23 +0900

    feat:乗車有無を登録するボタンを追加

--- frontend/where_child_bus_guardian/lib/pages/check_page/check_page.dart
+++ frontend/where_child_bus_guardian/lib/pages/check_page/check_page.dart
@@ -8,16 +8,43 @@ class CheckPage extends StatefulWidget {
 }
 
 class _CheckPageState extends State<CheckPage> {
+  bool isRideBus = true;
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            '乗車確認',
-          ),
-        ],
+        children: <Widget>[isRideBusToggleButton()],
+      ),
+    );
+  }
+
+  Widget isRideBusToggleButton() {
+    return Column(
+      children: [
+        toggleButtonBody("乗る", true),
+        const SizedBox(height: 20),
+        toggleButtonBody("乗らない", false),
+      ],
+    );
+  }
+
+  Widget toggleButtonBody(String text, bool isRide) {
+    return SizedBox(
+      width: 200,
+      height: 50,
+      child: ElevatedButton(
+        onPressed: () {
+          setState(() {
+            isRideBus = isRide;
+          });
+        },
+        style: ElevatedButton.styleFrom(
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(5))),
+        ),
+        child: Text(text),
       ),
     );
   }
