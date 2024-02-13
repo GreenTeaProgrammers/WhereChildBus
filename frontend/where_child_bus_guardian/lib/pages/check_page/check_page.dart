39e9b03dfb5fe35cbbbe58e1e685ccbdcc1bcac2
koto623 <akan.02toko@gmail.com>
2024-02-14 01:15:01 +0900

    move:現在時刻に関するファイルを移動

--- frontend/where_child_bus_guardian/lib/pages/check_page/check_page.dart
+++ frontend/where_child_bus_guardian/lib/pages/check_page/check_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_toggle_tab/flutter_toggle_tab.dart';
-import '../components/utils/current_time_body.dart';
+import '../../components/utils/current_time_body.dart';
 
 class CheckPage extends StatefulWidget {
   const CheckPage({super.key});
