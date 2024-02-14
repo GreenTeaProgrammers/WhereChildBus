8d62a370af5a640328bb2e1eac03a4faba3d46f5
KinjiKawaguchi <kawakin0310@icloud.com>
2024-02-14 19:00:10 +0900

    feat(api): Add DuplicationCheck RPC to ChildPhotoService

--- machine_learning/src/proto-gen/where_child_bus/v1/child_photo_pb2.pyi
+++ machine_learning/src/proto-gen/where_child_bus/v1/child_photo_pb2.pyi
@@ -5,6 +5,22 @@ from typing import ClassVar as _ClassVar, Iterable as _Iterable, Optional as _Op
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
+class DuplicationCheckRequest(_message.Message):
+    __slots__ = ("child_ids",)
+    CHILD_IDS_FIELD_NUMBER: _ClassVar[int]
+    child_ids: _containers.RepeatedScalarFieldContainer[str]
+    def __init__(self, child_ids: _Optional[_Iterable[str]] = ...) -> None: ...
+
+class DuplicationCheckResponse(_message.Message):
+    __slots__ = ("is_duplicated", "photo_ids", "duplicated_photos")
+    IS_DUPLICATED_FIELD_NUMBER: _ClassVar[int]
+    PHOTO_IDS_FIELD_NUMBER: _ClassVar[int]
+    DUPLICATED_PHOTOS_FIELD_NUMBER: _ClassVar[int]
+    is_duplicated: bool
+    photo_ids: _containers.RepeatedScalarFieldContainer[str]
+    duplicated_photos: _containers.RepeatedScalarFieldContainer[bytes]
+    def __init__(self, is_duplicated: bool = ..., photo_ids: _Optional[_Iterable[str]] = ..., duplicated_photos: _Optional[_Iterable[bytes]] = ...) -> None: ...
+
 class DeleteChildPhotoRequest(_message.Message):
     __slots__ = ("ids",)
     IDS_FIELD_NUMBER: _ClassVar[int]
