eb7707f13020b66e0d0708ad5a3e1478e36a4f07
Takuya Kataiwa <takuyakataiwa@icloud.com>
2024-02-15 23:02:43 +0900

    refactor: changed init methods

--- machine_learning/src/face_detect_model/data/faceDetectDataset.py
+++ machine_learning/src/face_detect_model/data/faceDetectDataset.py
@@ -2,10 +2,10 @@ import torch
 from torchvision import transforms
 import os
 import cv2
-from PIL import Image  # PILライブラリからImageクラスをインポート
+from PIL import Image
 
 class FaceDetectDataset(torch.utils.data.Dataset):
-    VALID_EXTENSIONS = {".png"}  # 変更しない
+    VALID_EXTENSIONS = {".png"}
 
     def __init__(self, config, transform=None):
         self.data_dir = config["dataset"]["root_path"]
@@ -27,36 +27,47 @@ class FaceDetectDataset(torch.utils.data.Dataset):
                 continue
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # BGRからRGBに変換
 
-            if name_label_dict.get(people_name) is None:
+            if people_name not in name_label_dict:
                 self.label_name_dict[self.label_num] = people_name
                 name_label_dict[people_name] = self.label_num
                 self.label_num += 1
+            
+            label = torch.tensor(name_label_dict[people_name], dtype=torch.int64)
+            image_pil = Image.fromarray(image)
 
-            self.face_data.append((
-                torch.tensor(name_label_dict[people_name], dtype=torch.int64),
-                image,  # ここではNumPy配列として格納
-            ))
+            self.face_data.append((label, self.transform(image_pil)))
+            
+            augmented_images = self.augment_image(image_pil, num_variations=5)
+            for aug_img in augmented_images:
+                self.face_data.append((label, aug_img))
 
     @staticmethod
     def default_transforms():
         return transforms.Compose([
-            transforms.ToPILImage(),  # NumPy配列をPIL Imageに変換
             transforms.Resize((224, 224)),
-            transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
 
     def augment_image(self, image, num_variations=5):
-        transformations = self.default_transforms()  # デフォルトの変換を使用
+    # ランダムな変換を適用するための拡張設定を強化
+        transformations = transforms.Compose([
+            transforms.RandomApply([transforms.Resize((256, 256)),
+                                    transforms.RandomCrop((224, 224))], p=0.5),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomVerticalFlip(p=0.5),
+            transforms.RandomApply([transforms.RandomRotation(degrees=15)], p=0.5),
+            transforms.RandomApply([transforms.RandomAffine(degrees=0, translate=(0.1, 0.1), scale=(0.8, 1.2))], p=0.5),
+            transforms.RandomApply([transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1)], p=0.5),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
         augmented_images = []
-
         for _ in range(num_variations):
-            # ここでランダムな変換を適用する
             augmented_image = transformations(image)
             augmented_images.append(augmented_image)
 
-        return torch.stack(augmented_images)  # テンソルのリストをスタックして返す
+        return augmented_images
 
     def _is_valid_file(self, file_name):
         return os.path.splitext(file_name)[1].lower() in self.VALID_EXTENSIONS
@@ -65,26 +76,10 @@ class FaceDetectDataset(torch.utils.data.Dataset):
         return len(self.face_data)
 
     def __getitem__(self, idx):
-        label, image_np = self.face_data[idx]
-        image = Image.fromarray(image_np)  # NumPy配列をPIL Imageに変換
-
-        # ここでaugment_imageメソッドを使用して画像のバリエーションを生成
-        augmented_images = self.augment_image(image)
-
-        # ラベルと拡張された画像のリストを紐付けて返す
-        return [(label, augmented_image) for augmented_image in augmented_images]
+        return self.face_data[idx]
 
     def get_label_name_dict(self):
         return self.label_name_dict
 
     def get_label_num(self):
         return self.label_num
-
-    def get_image_shape(self):
-        if len(self.face_data) > 0:
-            _, sample_image = self.face_data[0]
-            if self.transform:
-                sample_image = self.transform(sample_image)
-            return sample_image.shape
-        else:
-            return None
