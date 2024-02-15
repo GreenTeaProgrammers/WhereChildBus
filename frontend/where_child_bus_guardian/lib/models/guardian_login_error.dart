76d32a74c87d443310c4d2f658530e5807ec98c6
KikyoNanakusa <nanakusa.vrc@gmail.com>
2024-02-15 18:54:37 +0900

    refactor:AuthPageをリファクタリング

---
+++ frontend/where_child_bus_guardian/lib/models/guardian_login_error.dart
@@ -0,0 +1,8 @@
+enum GuardianLoginError {
+  unknown,
+  invalidCredentials,
+  serverError,
+  networkError,
+  databaseError,
+  // 他のエラータイプをここに追加
+}
--- frontend/where_child_bus_guardian/lib/pages/auth_page/auth_page.dart
+++ frontend/where_child_bus_guardian/lib/pages/auth_page/auth_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:where_child_bus_guardian/models/guardian_login_error.dart';
 
 class AuthPage extends StatefulWidget {
   const AuthPage({super.key});
@@ -12,63 +13,78 @@ class _AuthPageState extends State<AuthPage> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
 
+  GuardianLoginError? _loginError;
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(32),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'WhereChildBus',
-                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 32),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'メールアドレス',
-                    ),
-                    controller: _emailController,
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'パスワード',
-                    ),
-                    controller: _passwordController,
-                    obscureText: true,
-                    keyboardType: TextInputType.visiblePassword,
-                  ),
-                ),
-                const SizedBox(height: 32),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.6,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (kDebugMode) {
-                        debugPrint("email: ${_emailController.text}");
-                        debugPrint("password: ${_passwordController.text}");
-                      }
-                    },
-                    child: const Text('ログイン'),
-                  ),
-                )
-              ],
-            ),
-          ),
+        body: pageBody(),
+      ),
+    );
+  }
+
+  Widget pageBody() {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            titleText(),
+            mailInputField(),
+            passwordInputField(),
+            spacer32(),
+            if (_loginError != null &&
+                _loginError == GuardianLoginError.invalidCredentials)
+              emailOrPasswordNotFound(),
+            loginButton(),
+          ],
         ),
       ),
     );
   }
+
+  Widget titleText() => const Padding(
+        padding: EdgeInsets.only(bottom: 32),
+        child: Text('WhereChildBus',
+            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
+      );
+
+  Widget mailInputField() => Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextField(
+          decoration: const InputDecoration(
+              border: OutlineInputBorder(), labelText: 'メールアドレス'),
+          controller: _emailController,
+          keyboardType: TextInputType.emailAddress,
+        ),
+      );
+
+  Widget passwordInputField() => Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextField(
+          decoration: const InputDecoration(
+              border: OutlineInputBorder(), labelText: 'パスワード'),
+          controller: _passwordController,
+          obscureText: true,
+          keyboardType: TextInputType.visiblePassword,
+        ),
+      );
+
+  Widget spacer32() => const SizedBox(height: 32);
+
+  Widget emailOrPasswordNotFound() => const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Text(
+          "メールアドレスかパスワードが間違っています",
+          style: TextStyle(color: Colors.red, fontSize: 13),
+        ),
+      );
+
+  Widget loginButton() => SizedBox(
+        width: MediaQuery.of(context).size.width * 0.6,
+        child: ElevatedButton(onPressed: () {}, child: const Text('ログイン')),
+      );
 }
