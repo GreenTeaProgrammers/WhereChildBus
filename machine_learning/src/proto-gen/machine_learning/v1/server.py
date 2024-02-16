3c02983b18f213f25c7fe968470e1621aaa3bd26
Mizuki <mbaba@kanolab.net>
2024-02-17 01:32:01 +0900

    feat(ml): feat(ml): HealthCheckサービスの定義とMachineLearningサービスのFaceDetectAndClipメソッドの定義
    Squashed commit of the following:
    
    commit 73f6747d7148ac7df14eb6ffd734ba40622187c7
    Author: Mizuki <mbaba@kanolab.net>
    Date:   Sat Feb 17 01:23:54 2024 +0900
    
        feat(ml): HealthCheckサービスの定義とMachineLearningサービスのFaceDetectAndClipメソッドの定義
    
    commit dd246191de99b8967b6e67685b6328cd4b8c5e7a
    Author: Mizuki <mbaba@kanolab.net>
    Date:   Sat Feb 17 01:15:58 2024 +0900
    
        chore(ml): buf generateによって生成されたコードに対する変更

--- machine_learning/src/proto-gen/machine_learning/v1/server.py
+++ machine_learning/src/proto-gen/machine_learning/v1/server.py
@@ -1,35 +1,65 @@
+import argparse
+import logging
+import threading
 from concurrent import futures
+
 import grpc
+import health_check_pb2
+import health_check_pb2_grpc
 import machine_learning_pb2
 import machine_learning_pb2_grpc
 from grpc_reflection.v1alpha import reflection
 
+from face_detect_model.DetectFaceAndClip.detectFaceAndClip import main
 
-class HealthCheck(machine_learning_pb2_grpc.MachineLearningServiceServicer):
+
+class HealthCheckServiceServer(
+    machine_learning_pb2_grpc.MachineLearningServiceServicer
+):
     def Ping(self, request, context):
-        print("RECV: %s" % request.message)
-        message = "Hello, %s!" % request.message
+        print("RECV: %s" % request.name)
+        message = "Hello, %s!" % request.name
         print("SEND: %s" % message)
-        return machine_learning_pb2.PingResponse(message=message)
+        return health_check_pb2.PingResponse(message=message)
 
 
 class MachineLearningServiceServicer(
     machine_learning_pb2_grpc.MachineLearningServiceServicer
 ):
+    # TODO: implement Predict
     def Predict(self, request, context):
         pass
 
+    # TODO: implement Train
     def Train(self, request, context):
         pass
 
     def FaceDetectAndClip(self, request, context):
-        pass
+        parser = argparse.ArgumentParser()
+        args = parser.parse_args()
+
+        args.nursery_id = request.nursery_id
+        args.child_id = request.child_id
+        args.env = "remote"
+        # mainメソッドを別スレッドで実行
+        try:
+            thread = threading.Thread(target=main, args=(args,))
+            thread.start()
+            is_started = True
+        except Exception as e:
+            logging.error(e)
+            is_started = False
+
+        return machine_learning_pb2.FaceDetectAndClipResponse(is_started=is_started)
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    health_check_pb2_grpc.add_HealthcheckServiceServicer_to_server(
+        HealthCheckServiceServer(), server
+    )
     machine_learning_pb2_grpc.add_MachineLearningServiceServicer_to_server(
-        HealthCheck(), server
+        MachineLearningServiceServicer(), server
     )
 
     # Reflection APIの設定
@@ -37,14 +67,19 @@ def serve():
         machine_learning_pb2.DESCRIPTOR.services_by_name[
             "MachineLearningService"
         ].full_name,
+        health_check_pb2.DESCRIPTOR.services_by_name["HealthcheckService"].full_name,
         reflection.SERVICE_NAME,
     )
     reflection.enable_server_reflection(SERVICE_NAMES, server)
 
     server.add_insecure_port("[::]:8080")
     server.start()
+    logging.info("Server started at [::]:8080")
     server.wait_for_termination()
 
 
 if __name__ == "__main__":
+    # NOTE: https://github.com/grpc/grpc/issues/17321
+    # gRPCのPython環境でloggingを使う場合、server起動前にlogging.basicConfig()を実行する必要がある
+    logging.basicConfig()
     serve()
