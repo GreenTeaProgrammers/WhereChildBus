890a83fab1bbb27ead339eb89f68517ee17a9346
Takuya Kataiwa <takuyakataiwa@icloud.com>
2024-02-16 17:07:10 +0900

    refator: augmentation change

--- machine_learning/src/face_detect_model/data/faceDetectDataset.py
+++ machine_learning/src/face_detect_model/data/faceDetectDataset.py
@@ -9,7 +9,7 @@ class FaceDetectDataset(torch.utils.data.Dataset):
 
     def __init__(self, config, transform=None):
         self.data_dir = config["dataset"]["root_path"]
-        self.transform = transform if transform is not None else self.default_transforms()
+        default_transform = self.get_default_transforms()
         self.face_data = []
         self.label_name_dict = {}
         name_label_dict = {}
@@ -35,38 +35,40 @@ class FaceDetectDataset(torch.utils.data.Dataset):
             label = torch.tensor(name_label_dict[people_name], dtype=torch.int64)
             image_pil = Image.fromarray(image)
 
-            self.face_data.append((label, self.transform(image_pil)))
+            self.face_data.append((label, default_transform(image_pil)))
             
             augmented_images = self.augment_image(image_pil, num_variations=5)
             for aug_img in augmented_images:
                 self.face_data.append((label, aug_img))
 
-    @staticmethod
-    def default_transforms():
+    def get_default_transforms(self):
         return transforms.Compose([
             transforms.Resize((224, 224)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
-
-    def augment_image(self, image, num_variations=5):
-    # ランダムな変換を適用するための拡張設定を強化
-        transformations = transforms.Compose([
-            transforms.RandomApply([transforms.Resize((256, 256)),
-                                    transforms.RandomCrop((224, 224))], p=0.5),
+        
+    def get_augment_transform(self):
+        return transforms.Compose([
+            transforms.RandomApply([transforms.Resize((256, 256))], p=0.5),
+            transforms.RandomCrop((224, 224), p=0.5),
             transforms.RandomHorizontalFlip(p=0.5),
             transforms.RandomVerticalFlip(p=0.5),
-            transforms.RandomApply([transforms.RandomRotation(degrees=15)], p=0.5),
+            transforms.RandomApply([transforms.RandomRotation(degrees=180)], p=0.5),
             transforms.RandomApply([transforms.RandomAffine(degrees=0, translate=(0.1, 0.1), scale=(0.8, 1.2))], p=0.5),
             transforms.RandomApply([transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1)], p=0.5),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
+
+
+    def augment_image(self, image, num_variations=100):
+    # ランダムな変換を適用するための拡張設定を強化
+        transformations = self.get_augment_transform()
         augmented_images = []
         for _ in range(num_variations):
             augmented_image = transformations(image)
             augmented_images.append(augmented_image)
-
         return augmented_images
 
     def _is_valid_file(self, file_name):
