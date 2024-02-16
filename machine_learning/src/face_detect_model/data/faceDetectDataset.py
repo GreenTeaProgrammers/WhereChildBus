96b5c037fd35fecd0d451daf7f5516ab9422029d
Takuya Kataiwa <takuyakataiwa@icloud.com>
2024-02-16 17:17:22 +0900

    fixed bug

--- machine_learning/src/face_detect_model/data/faceDetectDataset.py
+++ machine_learning/src/face_detect_model/data/faceDetectDataset.py
@@ -43,7 +43,6 @@ class FaceDetectDataset(torch.utils.data.Dataset):
 
     def get_default_transforms(self):
         return transforms.Compose([
-            transforms.Resize((224, 224)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
