9e36b1fd6dc1837ea9e4e691897b7cdf5d17f857
KikyoNanakusa <nanakusa.vrc@gmail.com>
2024-02-21 22:15:14 +0900

    refactor:変数の受け渡しを変更

--- frontend/where_child_bus/lib/pages/camera_page/widgets/bus_select_box.dart
+++ frontend/where_child_bus/lib/pages/camera_page/widgets/bus_select_box.dart
@@ -4,32 +4,45 @@ import 'package:where_child_bus_api/proto-gen/where_child_bus/v1/resources.pb.da
 class BusSelectValueBox extends StatefulWidget {
   final List<Bus> busLists;
   final Function(Bus?) onChanged;
-  Bus? selectedBus;
+  final Bus? selectedBus;
 
-  BusSelectValueBox(
-      {required this.busLists, required this.onChanged, super.key});
+  const BusSelectValueBox({
+    required this.busLists,
+    required this.onChanged,
+    this.selectedBus,
+    super.key,
+  });
+
   @override
-  State<BusSelectValueBox> createState() => _SelectValueBox();
+  State<BusSelectValueBox> createState() => _BusSelectValueBoxState();
 }
 
-class _SelectValueBox extends State<BusSelectValueBox> {
+class _BusSelectValueBoxState extends State<BusSelectValueBox> {
+  Bus? _selectedBus;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedBus = widget.selectedBus ?? widget.busLists.first;
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: MediaQuery.of(context).size.width * 0.8,
       height: 50,
       child: DropdownButton<Bus>(
-          value: widget.selectedBus ?? widget.busLists.first,
-          items: widget.busLists
-              .map((Bus list) =>
-                  DropdownMenuItem(value: list, child: Text(list.name)))
-              .toList(),
-          onChanged: (Bus? value) {
-            setState(() {
-              widget.selectedBus = value;
-            });
-            widget.onChanged(value);
-          }),
+        value: _selectedBus,
+        items: widget.busLists.map((Bus bus) {
+          return DropdownMenuItem(value: bus, child: Text(bus.name));
+        }).toList(),
+        onChanged: (Bus? value) {
+          setState(() {
+            _selectedBus = value;
+          });
+          widget.onChanged(value);
+        },
+      ),
     );
   }
 }
