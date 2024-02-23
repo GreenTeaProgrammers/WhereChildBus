de7bdc8b0bad25becd131db28f87e89feb45275a
koto623 <akan.02toko@gmail.com>
2024-02-23 23:52:31 +0900

    chore: typoを修正

--- frontend/where_child_bus_guardian/ios/Runner/AppDelegate.swift
+++ frontend/where_child_bus_guardian/ios/Runner/AppDelegate.swift
@@ -1,7 +1,7 @@
 import UIKit
 import Flutter
 import GoogleMaps
-import GooglePlaces
+// import GooglePlaces
 import flutter_config
 
 @UIApplicationMain
@@ -10,8 +10,8 @@ import flutter_config
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSPlacesClient.provideAPI(FlutterConfigPlugin.env(for: "GOOGLE_MAP_API_KEY"))
-    GMSServices.provideAPI(FlutterConfigPlugin.env(for: "GOOGLE_MAP_API_KEY"))
+    // GMSPlacesClient.provideAPIKey(FlutterConfigPlugin.env(for: "GOOGLE_MAP_API_KEY"))
+    GMSServices.provideAPIKey(FlutterConfigPlugin.env(for: "GOOGLE_MAP_API_KEY"))
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
