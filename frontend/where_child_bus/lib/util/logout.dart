440d3219d7d6461aea12df0868c10526d57940df
KikyoNanakusa <nanakusa.vrc@gmail.com>
2024-02-23 21:34:34 +0900

    chore:不要なファイルを削除

--- frontend/where_child_bus/lib/util/logout.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:where_child_bus/pages/auth_page/auth_page.dart';
-
-class Logout {
-  static void logout(BuildContext context) {
-    Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (context) => const AuthPage()));
-  }
-}
