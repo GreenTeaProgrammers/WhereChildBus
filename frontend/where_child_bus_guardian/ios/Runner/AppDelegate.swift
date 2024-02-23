3c0ac6e1974c2169cb0dbb6d4f81ce47c24212e4
koto623 <akan.02toko@gmail.com>
2024-02-23 22:59:04 +0900

    fix: iosの設定と位置情報のパーミッションを設定

--- frontend/where_child_bus_guardian/ios/Runner/AppDelegate.swift
+++ frontend/where_child_bus_guardian/ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Flutter
+import GoogleMaps
+import flutter_config
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +9,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPI(FlutterConfigPlugin.env(for: "GOOGLE_MAP_API_KEY"))
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
