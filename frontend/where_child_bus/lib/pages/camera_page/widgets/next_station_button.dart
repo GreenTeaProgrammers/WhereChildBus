0af2d98bb9c0cc9e47f87a7ca56fc6f7e244ee22
KikyoNanakusa <nanakusa.vrc@gmail.com>
2024-02-23 18:28:56 +0900

    feat:nextStationボタンのコールバックを実装

--- frontend/where_child_bus/lib/pages/camera_page/widgets/next_station_button.dart
+++ frontend/where_child_bus/lib/pages/camera_page/widgets/next_station_button.dart
@@ -1,14 +1,13 @@
 import "dart:developer" as developer;
 import 'dart:async';
-import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:where_child_bus/util/api/bus.dart';
 import 'package:where_child_bus/util/api/busRoute.dart';
 import 'package:where_child_bus_api/proto-gen/where_child_bus/v1/resources.pb.dart';
 
 class NextStationButton extends StatefulWidget {
-  Bus bus;
-  BusType busType;
+  final Bus bus;
+  final BusType busType;
   Function(Bus) onPressed;
 
   NextStationButton(
@@ -27,13 +26,20 @@ class _NextStationButtonState extends State<NextStationButton> {
   bool _isLoading = false;
 
   Future<void> _fetchMorningBusRoute() async {
-    _isLoading = true;
+    if (mounted) {
+      setState(() {
+        _isLoading = true;
+      });
+    }
+
     try {
       final res = await getBusRouteByBusId(widget.bus.id, widget.busType);
-      setState(() {
-        _busRoute = res.busRoute;
-        _isLoading = false;
-      });
+      if (mounted) {
+        setState(() {
+          _busRoute = res.busRoute;
+          _isLoading = false;
+        });
+      }
     } catch (error) {
       developer.log("Caught Error By NextStationButton:",
           error: error, name: "NextStationButton");
@@ -57,6 +63,9 @@ class _NextStationButtonState extends State<NextStationButton> {
     } catch (error) {
       developer.log("Caught Error By NextStationButton:",
           error: error, name: "NextStationButton");
+    } finally {
+      if (!mounted) return;
+      setState(() => _isLoading = false);
     }
   }
 
@@ -70,10 +79,14 @@ class _NextStationButtonState extends State<NextStationButton> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8),
-      child: ElevatedButton(
-        onPressed: () {},
-        child: const Text("次の停留所へ"),
-      ),
+      child: _isLoading
+          ? const Center(
+              child: SizedBox(
+                  width: 20, height: 20, child: CircularProgressIndicator()))
+          : ElevatedButton(
+              onPressed: _updateNextStation,
+              child: const Text("次の停留所へ"),
+            ),
     );
   }
 }
